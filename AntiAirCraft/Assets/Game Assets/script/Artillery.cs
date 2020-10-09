b2562bbb11d14d420d1197fdc9e029654e90acfa
khazaee <mohsenkhazae@gmail.com>
2020-10-09 22:14:43 +0330

    99-07-18

---
+++ AntiAirCraft/Assets/Game Assets/script/Artillery.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Artillery : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform firePoint;
+    public GameObject fireEffect;
+    public int countShoot;
+
+    public void Fire()
+    {
+        if (countShoot < 2)
+        {
+            GameObject cloneprojectile = Instantiate(projectile, firePoint.transform.position, firePoint.transform.rotation);
+            cloneprojectile.GetComponent<Rigidbody>().AddForce(cloneprojectile.transform.forward * 5000, ForceMode.Acceleration);
+            fireEffect.GetComponent<ParticleSystem>().Play();
+            cloneprojectile.GetComponent<explosion>().Artillery = this;
+            countShoot++;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AntiAirCraft/Assets/Game Assets/script/explosion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour
+{
+    public Transform explosionPrefab;
+    public Artillery Artillery;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag== "AirCraft")
+        {
+            AirCraft air = collision.collider.gameObject.GetComponent<AirCraft>();
+            air.manageAirCraft.Lines[air.currentLine] = false;
+        }
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+        Instantiate(explosionPrefab, pos, rot);
+        Destroy(gameObject);
+        Destroy(collision.gameObject);
+    }
+    void OnBecameInvisible()
+    {
+        if (Artillery)
+        {
+            Artillery.countShoot--;
+            Destroy(gameObject);
+        }
+    }
+}
