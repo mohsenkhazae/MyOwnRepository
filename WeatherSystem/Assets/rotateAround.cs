7807f6290c9e3a1aa687a2358fd5d316d3c94343
khazaee <mohsenkhazae@gmail.com>
2020-10-19 16:37:41 +0330

    edite 99/7/28

---
+++ WeatherSystem/Assets/rotateAround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateAround : MonoBehaviour
+{
+    public float speed=1;
+    public GameObject planet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(planet.transform.position, Vector3.right, speed * Time.deltaTime);
+    }
+}
