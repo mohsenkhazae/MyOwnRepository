338fe6bc00bb408e85e2976ccbd3403e554a0a04
khazaee <mohsenkhazae@gmail.com>
2020-10-20 06:32:47 +0330

    7/29

--- WeatherSystem/Assets/rotateAround.cs
+++ WeatherSystem/Assets/rotateAround.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class rotateAround : MonoBehaviour
 {
-    public float speed=1;
+    public float MovementSpeed = 1;
+    public float radius=400;
+    public float timeRotate;
+    public float circumference;
     public GameObject planet;
     // Start is called before the first frame update
     void Start()
     {
-        
+        circumference = 2 * radius * Mathf.PI;
+        timeRotate = circumference / MovementSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(planet.transform.position, Vector3.right, speed * Time.deltaTime);
+        transform.RotateAround(planet.transform.position, Vector3.right, 360 * Time.deltaTime/ timeRotate);
     }
 }
--- WeatherSystem/Assets/rotateCloud.cs
+++ WeatherSystem/Assets/rotateCloud.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class rotateCloud : MonoBehaviour
 {
-    public float speed=1;
+    public float MovementSpeed=1;
+    public float radius = 400;
+    public float timeRotate;
+    public float circumference;
     // Start is called before the first frame update
     void Start()
     {
-        
+        circumference = 2 * radius * Mathf.PI;
+        timeRotate = circumference / MovementSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.Rotate(Vector3.forward * Time.deltaTime*speed);
+        gameObject.transform.Rotate(Vector3.forward * 360*Time.deltaTime/ timeRotate);
     }
 }
